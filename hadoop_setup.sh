2c1888001c5a8704ceb8d4e7650282f9df5cda85
Sean-YC <Kirito66605@gmail.com>
2024-11-13 22:09:44 -0500

    modify run script again

--- hadoop_setup.sh
+++ hadoop_setup.sh
@@ -3,6 +3,6 @@ git clone https://github.com/jen1902/14848-project.git && \
                     cd 14848-project && \
                     echo 'Ready to put data into hdfs' && \
                     hadoop fs -put input_data.txt / && \
-                    hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -file mapper.py -mapper '/opt/conda/default/bin/python mapper.py' -file reducer.py -reducer '/opt/conda/default/bin/python reducer.py' -input /test_data.txt -output /output1 && \
-                    hadoop fs -getmerge /output1 result && \
-                    cat result
+                    hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -file mapper.py -mapper '/opt/conda/default/bin/python mapper.py' -file reducer.py -reducer '/opt/conda/default/bin/python reducer.py' -input /input_data.txt -output /output2 && \
+                    hadoop fs -getmerge /output2 result1 && \
+                    cat result1
